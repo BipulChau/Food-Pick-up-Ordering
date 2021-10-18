e29c541c3bb7b6f79da382cef066c1d449e4fa62
Bipul Chaudhary <bipulchau@gmail.com>
2021-10-18 11:54:37 -0400

    Trying to figure out routing menu data

---
+++ db/seeds/01_menu.sql
@@ -0,0 +1,4 @@
+INSERT INTO menu (name, imglink, price, description, duration)
+VALUES('Espresso', 'https://github.com/BipulChau/midterm_project/blob/master/public/images/espresso.jpg?raw=true', 4, 'Very dark in flavor with a hint of caramel', 10),
+('Iced-Coffee', 'https://github.com/BipulChau/midterm_project/blob/master/public/images/iced-coffee.jpg?raw=true', 2.5, 'Handcrafted daily, made with a custom blend of cold-brewed coffee and 2% milk, sweetened with pure cane sugar and served over ice.', 5),
+('Latte', 'https://github.com/BipulChau/midterm_project/blob/master/public/images/latte.jpeg?raw=true', 5, 'Lots of steamed milk with espresso shots to liven it up, usually topped with about 1/2 inch or so of foam.', 15);
